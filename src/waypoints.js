4baad74e0968bdcb9d80069472ebd48f5cf800dd
Jon Tewksbury <jontewks@gmail.com>
2015-10-31 10:55:33 -0700

    Add waypoints and working promise based distance calculation with gmaps distance matrix

---
+++ src/waypoints.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = [
+	'Russian River Brewing Company, 4th Street, Santa Rosa, CA',
+	'Chuckanut Brewery and Kitchen, Bellingham, WA',
+	'Elysian Brewery, Seattle, WA',
+	// 'Breakside Brewery, Portland, OR',
+	// 'AleSmith Brewery, San Diego, CA',
+	// 'Thai Me Up Brewery, Jackson, WY',
+	// 'Firestone Walker Brewery, Paso Robles, CA',
+	// 'New Belgium Brewery, Fort Collins, CO',
+	// 'Avery Brewing Co., Boulder, CO',
+	// 'Crooked Stave Artisan Beer Project, Denver, CO',
+	// 'Sierra Nevada Brewery, Chico, CA',
+	// 'Pizza Port, Carlsbad, CA',
+	// 'Ballast Point Brewery, San Diego, CA',
+	// 'Upright Brewing, Portland, OR',
+	// 'Deschutes Brewery, Bend, OR',
+]
