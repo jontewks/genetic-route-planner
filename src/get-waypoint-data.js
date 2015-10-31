83afe88cd35646bbc0a8d47e036e8b2209bc9576
Jon Tewksbury <jontewks@gmail.com>
2015-10-31 13:59:00 -0700

    Change variable and file names, save responses from google to json file

---
+++ src/get-waypoint-data.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Promise = require('bluebird')
+const distance = Promise.promisifyAll(require('google-distance'))
+const writeFile = Promise.promisify(require('fs').writeFile)
+
+const waypoints = require('../config').waypoints
+const createCombos = require('../util/create-combinations')
+
+const waypointCombos = createCombos(waypoints)
+const distances = []
+
+waypointCombos.forEach(combo => {
+	distances.push(distance.getAsync({
+		origin: combo[0],
+		destination: combo[1],
+	}))
+})
+
+Promise.all(distances)
+	.then(data => {
+		return writeFile('src/waypoint-data.json', JSON.stringify(data))
+	})
